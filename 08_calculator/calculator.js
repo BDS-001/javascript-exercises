bcdaaeeb161cef02c45551e41f924d546fb75938
BDS <113209967+BDS-001@users.noreply.github.com>
2023-08-22 16:44:35 -0400

    passed all 08 calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  
+  if (nums.length === 0) {
+    return 0
+  } else {
+    return nums.reduce((total, num) => {
+      return total + num
+    }, 0)
+  };
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  if (nums.length === 0) {
+    return 0
+  } else {
+    let total = nums[0]
+    nums.splice(0, 1)
+    for (i=0;i<nums.length;i++) {
+      total *= nums[i]
+    }
+    return total
+  };
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let total = base
+  for (i=1;i<exponent;i++) {
+    total *= base;
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+  if (factor === 0) {
+    return 1
+  }
+
+	total = factor
+  factor -= 1
+  for (i=factor;i>0;i--) {
+    total *= i
+  }
+  return total
 };
 
 // Do not edit below this line
