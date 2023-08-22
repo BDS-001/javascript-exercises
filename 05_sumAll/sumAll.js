6c2474f67d61cfdbcf0be70a0ba453737ec310a6
BDS <113209967+BDS-001@users.noreply.github.com>
2023-08-22 17:34:19 -0400

    retested 2-7 by removing skip in the npm test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,15 @@
 const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) return 'ERROR';
+
     let total = 0;
-    for (i=start;i<=end;i++) {
-        total += i
+    if (start < end) {
+        for (i=start;i<=end;i++) {
+            total += i
+        }
+    } else {
+        for (i=end;i<=start;i++) {
+            total += i
+        }
     }
     return total
 };
