5fe8bb3741b6ae23b5b27803013984267dceb469
BDS <113209967+BDS-001@users.noreply.github.com>
2023-08-23 14:04:51 -0400

    completed 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    reverse = ''
+    for (i=text.length - 1; i >= 0; i--) {
+        reverse += text[i]
+    }
+    return text === reverse
 };
 
 // Do not edit below this line
