e7ef39ee741ee13f8314ce11184c9ef8cd4a5e8f
BDS <113209967+BDS-001@users.noreply.github.com>
2023-08-23 14:18:05 -0400

    completed 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 1) return 'OOPS';
+    let a = 0
+    let b = 1
 
+    for (i=1;i < num;i++) {
+        let tmp = b
+        b += a
+        a = tmp
+    }
+    return b
 };
 
 // Do not edit below this line
