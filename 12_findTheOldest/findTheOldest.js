f1e8f8edab2c525ab33bae2d2d1ebf340971edf3
BDS <113209967+BDS-001@users.noreply.github.com>
2023-08-23 19:09:04 -0400

    completed 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    
+    let oldest = people.sort((personA, personB) => {
+        let deathA = personA.yearOfDeath
+        let deathB = personB.yearOfDeath
+
+        if (deathA === undefined) deathA = (new Date()).getFullYear();
+        if (deathB === undefined) deathB = (new Date()).getFullYear();
+
+        const ageA = deathA - personA.yearOfBirth
+        const ageB = deathB - personB.yearOfBirth
+
+        if (ageA > ageB) {
+            return -1
+        } else {
+            return 1
+        }
+    })
+
+    return oldest[0]
 
 };
 
